f9da3d885c53322df235c84b27850dbc90996ccb
otaviozerotwo <deadplatypus@protonmail.com>
2023-06-03 16:52:47 -0300

    ajuste index.html e função que gera as capas dinamicamente

--- js/main.js
+++ js/main.js
@@ -6,19 +6,25 @@ function ajustar_resolucao(){
       $('.cabecalho__busca').show();
     }
 
-    if($(window).width() <= 1300){
+    if($(window).width() <= 1400){
+      $('.capa_8').hide();
+    }else{
+      $('.capa_8').show();
+    }
+
+    if($(window).width() <= 1250){
       $('.capa_7').hide();
     }else{
       $('.capa_7').show();
     }
 
-    if($(window).width() <= 1150){
+    if($(window).width() <= 1050){
       $('.capa_6').hide();
     }else{
       $('.capa_6').show();
     }
 
-    if($(window).width() <= 950){
+    if($(window).width() <= 900){
       $('.capa_5').hide();
     }else{
       $('.capa_5').show();
@@ -30,7 +36,7 @@ function ajustar_resolucao(){
       $('.capa_4').show();
     }
 
-    if($(window).width() <= 580){
+    if($(window).width() <= 550){
       $('.capa_3').hide();
     }else{
       $('.capa_3').show();
@@ -86,12 +92,6 @@ function hover_manga(){
   $('#info_adicionais_6').hide();
   $('#info_adicionais_7').hide();
   $('#info_adicionais_8').hide();
-  $('#info_adicionais_9').hide();
-  $('#info_adicionais_10').hide();
-  $('#info_adicionais_11').hide();
-  $('#info_adicionais_12').hide();
-  $('#info_adicionais_13').hide();
-  $('#info_adicionais_14').hide();
 
   $('#img_capa_1').hover(function(){
     $('#info_adicionais_1').show();
@@ -140,46 +140,36 @@ function hover_manga(){
   }, function(){
     $('#info_adicionais_8').hide();
   });
-
-  $('#img_capa_9').hover(function(){
-    $('#info_adicionais_9').show();
-  }, function(){
-    $('#info_adicionais_9').hide();
-  });
-
-  $('#img_capa_10').hover(function(){
-    $('#info_adicionais_10').show();
-  }, function(){
-    $('#info_adicionais_10').hide();
-  });
-
-  $('#img_capa_11').hover(function(){
-    $('#info_adicionais_11').show();
-  }, function(){
-    $('#info_adicionais_11').hide();
-  });
-
-  $('#img_capa_12').hover(function(){
-    $('#info_adicionais_12').show();
-  }, function(){
-    $('#info_adicionais_12').hide();
-  });
-
-  $('#img_capa_13').hover(function(){
-    $('#info_adicionais_13').show();
-  }, function(){
-    $('#info_adicionais_13').hide();
-  });
-
-  $('#img_capa_14').hover(function(){
-    $('#info_adicionais_14').show();
-  }, function(){
-    $('#info_adicionais_14').hide();
-  });
 }
+
+function adicionar_capas(){
+    var nome_div = $('.home__capas'); // Seletor para a div com a classe home__capas
+    
+    // Verifica se a div já possui a tag <ul>
+    var nova_tag_ul = $('<ul>'); // Cria a tag <ul>
+      
+    for(var i = 1; i <= 8; i++){
+    // Cria a tag <li> com o conteúdo desejado
+      var nova_tag_li = $('<li>').addClass('capa_' + i)
+                                  .append($('<img>').attr('src', 'img/dragon_ball_' + i + '.png')
+                                                    .attr('id', 'img_capa_' + i)
+                                                    .attr('alt', 'Capa Dragon Ball #' + i))
+                                  // .append($('<div>').attr('id', 'info_adicionais_' + i)
+                                  //                   .append($('<p>').text('Informações adicionais')))
+                                  .append($('<a>').attr('href', '#').text('+ COLEÇÃO'))
+                                  .append($('<p>').text('Dragon Ball #' + i));
+
+      // Adiciona a nova tag <li> à tag <ul>
+      nova_tag_ul.append(nova_tag_li);
+    }
+    // Adiciona a tag <ul> à div com a classe home__capas
+    nome_div.append(nova_tag_ul);
+}
+
 
 $(document).ready(function(){
   mostrar_descricao_menu();
+  adicionar_capas();
   ajustar_resolucao();
-  hover_manga();
+  // hover_manga();
 });
